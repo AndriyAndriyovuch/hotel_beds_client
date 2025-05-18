b75ae6759b7e116faae391789b6b0d6cad4ddeb6
AndriyAndriyovuch <andriy.mazurok.tl@gmail.com>
2025-05-18 14:55:57 +0300

    Initial commit

---
+++ lib/hotel_beds_client.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "hotel_beds_client/version"
+
+module HotelBedsClient
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/hotel_beds_client/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module HotelBedsClient
+  VERSION = "0.1.0"
+end
