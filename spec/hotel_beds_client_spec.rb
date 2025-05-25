62a0810a996ff60170a057da4e78c10651f2a272
AndriyAndriyovuch <andriy.mazurok.tl@gmail.com>
2025-05-25 13:41:41 +0300

    Added tests for .hotels_list

--- spec/hotel_beds_client_spec.rb
+++ spec/hotel_beds_client_spec.rb
@@ -1,7 +1,35 @@
 # frozen_string_literal: true
 
 RSpec.describe HotelBedsClient do
-  it 'has a version number' do
-    expect(HotelBedsClient::VERSION).to be '0.1.0'
+  describe '.hotels_list' do
+    subject(:res) { HotelBedsClient.hotels_list }
+
+    before do
+      request_stub = JSON.parse(File.read('spec/shared_context/stubs/hotels_list_stub.json'))
+
+      allow(HotelBedsClient::HotelsList::Request).to receive(:call).and_return(request_stub)
+    end
+
+    it 'returns correct class' do
+      expect(res).to be_a(HotelBedsClient::HotelContent::HotelsList::Response)
+    end
+
+    it 'returns correct keys' do
+      expect(res.instance_variables).to contain_exactly(:@json, :@from, :@to, :@total, :@hotels_json, :@hotels)
+    end
+
+    it 'returns correct value types' do
+      expect(res.json).to be_a(Hash)
+
+      expect(res.from).to be_a(Integer)
+      expect(res.to).to be_a(Integer)
+      expect(res.total).to be_a(Integer)
+
+      expect(res.hotels_json).to be_a(Array)
+      expect(res.hotels_json.first).to be_a(Hash)
+
+      expect(res.hotels).to be_a(Array)
+      expect(res.hotels.first).to be_a(HotelBedsClient::HotelContent::HotelsList::Hotel)
+    end
   end
 end
